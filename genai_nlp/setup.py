83c27ffb788b5238cc9bfa482623142d4ce81d0e
dzmitry ashkinadze <dzmitry.ashkinadze@gmail.com>
2023-08-18 21:16:23 +0200

    initial commit

---
+++ genai_nlp/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='genai_nlp',
+    version='0.1.0',    
+    description='A example Python package',
+    author='Dima',
+    author_email='test@test.gmail',
+    packages=['genai_nlp'],
+    install_requires=[
+        'numpy',                     
+    ]
+)
